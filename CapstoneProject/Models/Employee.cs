ddf0819a3503180e9b4be26c42aa37e6294eacc3
Will <uwillia1@my.westga.edu>
2017-02-10 18:01:32 -0500

    Model.
    Started Model classes. Subject to change.

---
+++ CapstoneProject/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int CohortID { get; set; }
+        public Cohort Cohort { get; set; }
+        public int SupervisorID { get; set; }
+        public Employee Supervisor { get; set; }
+        public int ManagerID { get; set; }
+        public Employee Manager { get; set; }
+    }
+}
