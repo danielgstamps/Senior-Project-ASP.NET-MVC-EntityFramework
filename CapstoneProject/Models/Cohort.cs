588225f425cefaf75082642952110f1cb2cd8079
Will <uwillia1@my.westga.edu>
2017-02-10 18:25:09 -0500

    Cohort Model.
    Added properties to Cohort.

--- CapstoneProject/Models/Cohort.cs
+++ CapstoneProject/Models/Cohort.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace CapstoneProject.Models
 {
     public class Cohort
     {
+        private const int MAX_NAME_LENGTH = 50;
+
+        [Display(Name = "Cohort ID")]
+        [Range(0, int.MaxValue, ErrorMessage = "ID must be a non-negative whole number")]
+        [Required(ErrorMessage = "Cohort ID required")]
+        public int CohortID { get; set; }
+
+        [Display(Name = "Cohort Names")]
+        [StringLength(MAX_NAME_LENGTH)]
+        [Required(ErrorMessage = "Cohort name required")]
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
     }
 }
