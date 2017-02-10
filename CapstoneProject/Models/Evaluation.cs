88af8ac406363bdcffc90814a9a6c65576a8f890
Will <uwillia1@my.westga.edu>
2017-02-10 18:34:15 -0500

    Model.
    Added Category and Evaluation.

---
+++ CapstoneProject/Models/Evaluation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models
+{
+    public class Evaluation
+    {
+        [Display(Name = "Evaluation ID")]
+        [Range(0, int.MaxValue, ErrorMessage = "ID must be a non-negative whole number")]
+        [Required(ErrorMessage = "Evaluation ID required")]
+        public string EvaluationID { get; set; }
+
+        public string CategoryID { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
