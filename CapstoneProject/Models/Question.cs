9e34ff6ac080f547506f8ef5848e9e8dd013bdec
Will <uwillia1@my.westga.edu>
2017-02-14 11:55:57 -0500

    Model.
    Wrote Question model.

---
+++ CapstoneProject/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models
+{
+    public class Question
+    {
+        private const int MAX_LENGTH = 500;
+
+        [Display(Name = "Question ID")]
+        [Range(0, int.MaxValue, ErrorMessage = "ID must be a non-negative whole number")]
+        [Required(ErrorMessage = "Question ID required")]
+        public int QuestionID { get; set; }
+
+        [Display(Name = "Question")]
+        [StringLength(MAX_LENGTH)]
+        [Required(ErrorMessage = "Must have question")]
+        public string QuestionText { get; set; }
+        
+        [Display(Name = "Comment")]
+        [StringLength(MAX_LENGTH)]
+        public string Comment { get; set; }
+    }
+}
