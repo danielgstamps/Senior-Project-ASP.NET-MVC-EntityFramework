50dfa868bbfc4b04e107f6e5e343199a24c50921
Will <uwillia1@my.westga.edu>
2017-02-14 15:13:05 -0800

    Context.
    Wrote context for Evaluation and related models. Should be ready for migrations now.

---
+++ CapstoneProject/DAL/EvaluationContext.cs
@@ -0,0 +1,28 @@
+﻿using CapstoneProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.DAL
+{
+    public class EvaluationContext : DbContext
+    {
+        public EvaluationContext() : base("EvaluationContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cohort> Cohorts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        } 
+    }
+}
