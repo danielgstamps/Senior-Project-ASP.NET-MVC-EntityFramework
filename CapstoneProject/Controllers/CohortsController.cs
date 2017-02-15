4022c9a267fe86a4d68f9491c0520900bc033c0b
Will <uwillia1@my.westga.edu>
2017-02-15 11:43:25 -0500

    Controllers, View, and Navigation.
    Created Controllers and Views for Cohorts, Employees, and Evaluations.
    
    Made links in the nav bar navigable.

---
+++ CapstoneProject/Controllers/CohortsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapstoneProject.DAL;
+using CapstoneProject.Models;
+
+namespace CapstoneProject.Controllers
+{
+    public class CohortsController : Controller
+    {
+        private EvaluationContext db = new EvaluationContext();
+
+        // GET: Cohorts
+        public ActionResult Index()
+        {
+            return View(db.Cohorts.ToList());
+        }
+
+        // GET: Cohorts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cohort cohort = db.Cohorts.Find(id);
+            if (cohort == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cohort);
+        }
+
+        // GET: Cohorts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CohortID,Name")] Cohort cohort)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cohorts.Add(cohort);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cohort);
+        }
+
+        // GET: Cohorts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cohort cohort = db.Cohorts.Find(id);
+            if (cohort == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cohort);
+        }
+
+        // POST: Cohorts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CohortID,Name")] Cohort cohort)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cohort).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cohort);
+        }
+
+        // GET: Cohorts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cohort cohort = db.Cohorts.Find(id);
+            if (cohort == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cohort);
+        }
+
+        // POST: Cohorts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cohort cohort = db.Cohorts.Find(id);
+            db.Cohorts.Remove(cohort);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
