97cc270f0673c4e47d34c6a2c8fcb5472fb383e0
William Underwood <uwillia1@my.westga.edu>
2017-02-21 23:53:29 -0800

    Tests.
    Concluded testing for models.

---
+++ CapstoneProjectTests/EvaluationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapstoneProject.Models;
+
+namespace CapstoneProjectTests
+{
+    [TestClass]
+    public class EvaluationTest
+    {
+        private Evaluation evaluation;
+        private Employee employee;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.evaluation = new Evaluation();
+            this.evaluation.EvaluationID = 1;
+            this.employee = new Employee();
+            this.employee.EmployeeID = 2;
+        }
+
+        [TestMethod]
+        public void TestEvaluationHasEmployeeID()
+        {
+            this.evaluation.EmployeeID = 2;
+            Assert.AreEqual(employee.EmployeeID, this.evaluation.EmployeeID);
+        }
+
+        [TestMethod]
+        public void TestEvaluationHasEmployee()
+        {
+            this.evaluation.Employee = this.employee;
+            Assert.AreEqual(this.employee, this.evaluation.Employee);
+        }
+
+        [TestMethod]
+        public void TestEvaluationDoesNotHaveEmployeeID()
+        {
+            this.evaluation.EmployeeID = 3;
+            Assert.AreNotEqual(this.employee.EmployeeID, this.evaluation.EmployeeID);
+        }
+
+        [TestMethod]
+        public void TestEvaluationDoesNotHaveEmployee()
+        {
+            this.evaluation.Employee = new Employee();
+            Assert.AreNotSame(this.employee, this.evaluation.Employee);
+        }
+    }
+}
---
+++ CapstoneProjectTests/QuestionTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CapstoneProject.Models;
+using System.Collections.Generic;
+
+namespace CapstoneProjectTests
+{
+    [TestClass]
+    public class QuestionTest
+    {
+        private Category category;
+        private Question question;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.category = new Category();
+            this.question = new Question();
+            this.category.Questions = new List<Question>();
+            this.category.Questions.Add(this.question);
+            this.question.Categories = new List<Category>();
+            this.question.Categories.Add(this.category);
+        }
+
+        [TestMethod]
+        public void TestCategoryHasQuestion()
+        {
+            Assert.IsTrue(this.category.Questions.Contains(this.question));
+        }
+
+        [TestMethod]
+        public void TestCategoryDoesNotHaveQuestion()
+        {
+            Assert.IsFalse(this.category.Questions.Contains(new Question()));
+        }
+
+        [TestMethod]
+        public void TestQuestionHasCategory()
+        {
+            Assert.IsTrue(this.question.Categories.Contains(this.category));
+        }
+
+        [TestMethod]
+        public void TestQuestionDoesNotHaveCategory()
+        {
+            Assert.IsFalse(this.question.Categories.Contains(new Category()));
+        }
+    }
+}
