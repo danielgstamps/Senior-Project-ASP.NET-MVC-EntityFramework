72e96e108ae6a246d1614dd837f7432df0e1127d
Will Underwood <uwillia1@my.westga.edu>
2017-03-04 17:14:41 -0500

    Adding missing files.

---
+++ CapstoneProject/DAL/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CapstoneProject.Models;
+
+namespace CapstoneProject.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private DataContext context = new DataContext();
+        private GenericRepository<Employee> employeeRepository;
+        private GenericRepository<Cohort> cohortRepository;
+        private bool disposed = false;
+
+        public GenericRepository<Employee> EmployeeRepository
+        {
+            get
+            {
+                if (this.employeeRepository == null)
+                {
+                    this.employeeRepository = new GenericRepository<Employee>(this.context);
+                }
+                return employeeRepository;
+            }
+        }
+
+        public GenericRepository<Cohort> CohortRepository
+        {
+            get
+            {
+                if (this.cohortRepository == null)
+                {
+                    this.cohortRepository = new GenericRepository<Cohort>(this.context);
+                }
+                return cohortRepository;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+    }
+}
