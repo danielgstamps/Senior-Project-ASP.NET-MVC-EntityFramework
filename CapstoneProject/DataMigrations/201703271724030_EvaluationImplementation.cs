edf3f3fb76a19f9b7bd7394bcbf38ba3f8b2cf33
William Underwood <uwillia1@my.westga.edu>
2017-03-27 13:32:54 -0400

    Added missing files.

---
+++ CapstoneProject/DataMigrations/201703271724030_EvaluationImplementation.cs
@@ -0,0 +1,16 @@
+namespace CapstoneProject.DataMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EvaluationImplementation : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
