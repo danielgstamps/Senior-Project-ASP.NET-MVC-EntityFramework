b207eea0fe7c02a95d4c4b2729d6d128eb88dcd4
William Underwood <uwillia1@my.westga.edu>
2017-03-29 00:58:48 -0700

    Moq.
    Installed Moq on CapstoneProjectTests.

---
+++ CapstoneProjectTests/Models/InMemoryGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneProject.DAL;
+
+namespace CapstoneProjectTests.Models
+{
+    public class InMemoryGenericRepository<TEntity> : GenericRepository<TEntity> where TEntity : class
+    {
+        public InMemoryGenericRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
