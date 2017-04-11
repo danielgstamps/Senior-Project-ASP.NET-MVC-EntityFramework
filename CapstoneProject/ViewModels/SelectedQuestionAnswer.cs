fd8dc167583a83b81385b76f90e41ae750a13342
CHUMA92 <cokafor3@hotmail.com>
2017-04-11 17:32:46 -0400

    Added SelectedQuestionAnswer.cs view model class

---
+++ CapstoneProject/ViewModels/SelectedQuestionAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CapstoneProject.Models;
+
+namespace CapstoneProject.ViewModels
+{
+    public class SelectedQuestionAnswer
+    {
+        public Question Question { get; set; }
+
+        [Required]
+        public string SelectedAnswer { get; set; }
+    }
+}
