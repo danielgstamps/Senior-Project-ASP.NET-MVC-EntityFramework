e6645202760b0982e25110ae50ff3847926ae080
William Underwood <wwunderwood93@gmail.com>
2017-04-21 17:57:27 -0400

    Added PdfController with example methods. Methods need to be changed later on to generate reports.

---
+++ CapstoneProject/Controllers/PdfController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using iTextSharp.text;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcRazorToPdf;
+
+namespace CapstoneProject.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+        public ActionResult Index()
+        {
+            var anon = new
+            {
+                Output = "Print me!"
+            };
+            return new PdfActionResult(anon);
+        }
+
+        /// <summary>
+        /// Writes a pdf to application data directory.
+        /// </summary>
+        public void WriteToAppData()
+        {
+            var anon = new
+            {
+                Output = "Write me to a pdf in the application data directory"
+            };
+            byte[] pdfOutput = ControllerContext.GeneratePdf(anon, "");
+            string fullPath = Server.MapPath("~/App_Data/MadeByPdfController.pdf");
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+            System.IO.File.WriteAllBytes(fullPath, pdfOutput);
+        }
+
+        public ActionResult IndexWithAccessToDocumentAndWriter()
+        {
+            var anon = new
+            {
+                Output = "IndexWithAccessToDocumentAndWriter"
+            };
+            return new PdfActionResult(anon, (writer, document) =>
+            {
+                document.SetPageSize(new Rectangle(500f, 500f, 90));
+                document.NewPage();
+            });
+        }
+
+        public ActionResult DownloadPDF()
+        {
+            var anon = new
+            {
+                Output = "Download me!"
+            };
+            return new PdfActionResult(anon, (writer, document) =>
+            {
+                document.SetPageSize(new Rectangle(500f, 500f, 90));
+                document.NewPage();
+            })
+            {
+                FileDownloadName = "DownloadMe.pdf"
+            };
+        }
+
+    }
+}
