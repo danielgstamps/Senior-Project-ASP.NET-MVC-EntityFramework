b8f6c9e28fa0453c4b2417ff2c1d1e4802ba4624
William Underwood <wwunderwood93@gmail.com>
2017-04-22 18:46:04 -0400

    Added commas to seperate answers.

---
+++ CapstoneProject/ViewModels/EvaluationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.ViewModels
+{
+    public class EvaluationData
+    {
+        public string SelfAnswers { get; set; }
+        public int NumberOfSupervisors { get; set; }
+        public int NumberOfCoworkers { get; set; }
+        public int NumberOfSupervisees { get; set; }
+        public string[] SupervisorAnswers { get; set; }
+        public string[] CoworkerAnswers { get; set; }
+        public string[] SuperviseeAnswers { get; set; }
+    }
+}
