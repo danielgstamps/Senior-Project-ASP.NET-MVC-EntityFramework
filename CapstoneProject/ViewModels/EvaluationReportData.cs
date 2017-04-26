6b4f7a1871078388c28df331b990845675cc3727
csuser <csuser@CS1117W03>
2017-04-26 14:04:29 -0400

    Changed Report.cshtml to take EvaluationReportData instead of Evaluation. Averages are calculated correctly for a single supervisor with no other raters.

--- CapstoneProject/ViewModels/EvaluationReportData.cs
+++ CapstoneProject/ViewModels/EvaluationReportData.cs
@@ -8,10 +8,13 @@ namespace CapstoneProject.ViewModels
 {
     public class EvaluationReportData
     {
+        public int EvaluationID { get; set; }
         public string EmployeeName { get; set; }
         public string TypeName { get; set; }
         public string StageName { get; set; }
         public string CurrentDate { get { return DateTime.Today.ToString().Split(' ')[0]; } }
+        public List<Rater> Raters { get; set; }
+        public List<Category> Categories { get; set; }
         public List<int> EmployeeAnswers { get; set; }
         public List<int> SupervisorAvgAnswers { get; set; }
         public List<int> CoworkerAvgAnswers { get; set; }
